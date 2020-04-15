e52ec40bfac7916a9037c24f10ac5934fea0c9e8
unatalbarbara <unatalbarbara@gmail.com>
2020-04-15 20:53:20 +0200

    python start

---
+++ Calculator.py
@@ -0,0 +1,5 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another a number: ")
+# resultado = int(num1) + int(num2)
+resultado = float(num1) + float(num2)
+print(round(resultado))
--- index.py
+++ index.py
@@ -1,2 +1,62 @@
-output ="Hola mundo!"
-print(hola mundo)
+from math import *
+
+# como importar cosas
+output = 'Hola mundo'
+print("Hola mundo!")
+
+# /*    public static String nif(int dni) {
+#         char char = "TRWAGMYFPDXBNJZSQVHLCKE".charAt(dni % 23);
+#         return Integer.toString(dni) + charDni;
+
+# test = "kappa"
+# elem = test[0] # charAt(0), which is 'k' in this case
+#     }
+number1 = 0
+number2 = 55.45554
+if number1 > number2:
+    print(number1)
+else:
+    print(number2)
+string = "jPython"
+
+print(string.upper() + "\n", number2)
+
+print(len(string))
+
+print(string[0] + string[5])
+
+print(string.index("P"))  # Guay
+
+# Replace funtion
+animales = "Girafa Academy"
+# Reemplazar valores
+print(animales.replace("Girafa", "Elefante"))
+
+print(4 / 2)
+num = -4.50255
+# convertir numero en string
+print(str(num))
+# Valor absoluto, nu mar negativos?
+# str(num)
+num = 5
+
+print(pow(num, 2))
+
+print(round(num))
+
+print(floor(3.7))  # Nearest [3]
+print(ceil(3.5))  # farest [4]
+
+# raiz cuadrada
+
+print(sqrt(9).__round__())  # perfecto
+
+# Input from users
+
+DNI = input("Introduce tu DNI: ")
+print(DNI)
+
+name = input("Introduce tu nombre: ")
+
+print(name, DNI)
+
