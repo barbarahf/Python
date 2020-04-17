9845f128788b1bbdc48c7a8e8848380d4ee6c659
unatalbarbara <unatalbarbara@gmail.com>
2020-04-17 07:14:28 +0200

    funciones python

--- listas.py
+++ listas.py
@@ -1,1 +1,34 @@
-friend = ["Kevin", "Karen", "Jim"]
+friend = ["Kevin", "Karen", "Jim", "Ana", "Kevins", "Jimie", "Paloma"]
+lucky_number = [4, 8, 15, 16, 23, 42]
+# print(friend[1:3])
+# friend.extend(lucky_number) #Concatenar arrays
+friend.append("Oliver")  # Añadir valores
+friend.insert(1, "oh")  # reemplazar
+print(friend)
+# Eliminar valores
+friend.remove("oh")
+print(friend)
+# Pop -> Elimina el ultimo elemento
+friend.pop()
+
+print(friend)
+
+print(friend.count("Kevins"))
+
+friend.sort()
+
+lucky_number.sort()
+
+print(friend)
+
+print(lucky_number)
+
+lucky_number.reverse()
+
+print(lucky_number)
+
+frinds2 = friend.copy()
+
+# Tople
+
+coordenadas = (4, 5) #No s epueden modificar, let
