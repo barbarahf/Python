360330d9b2d8fe467efee3835b5284d3304be640
unatalbarbara <unatalbarbara@gmail.com>
2020-04-22 04:47:07 +0200

    udemy tutorial

---
+++ funciones.py
@@ -0,0 +1,7 @@
+def inicial_funciton(name):
+    print("Hey " + name)
+
+
+# Llamar funcion
+nombre = input("Introduce tu nombre: ")
+inicial_funciton(nombre)
--- index.py
+++ index.py
@@ -1,70 +1,70 @@
-# # from math import *
-# #
-# # # como importar cosas
-# # output = 'Hola mundo'
-# # print("Hola mundo!")
-# #
-# # number1 = 0
-# # number2 = 55.45554
-# # if number1 > number2:
-# #     print(number1)
-# # else:
-# #     print(number2)
-# # string = "jPython"
-# #
-# # print(string.upper() + "\n", number2)
-# #
-# # print(len(string))
-# #
-# # print(string[0] + string[5])
-# #
-# # print(string.index("P"))  # Guay
-# #
-# # # Replace funtion
-# # animales = "Girafa Academy"
-# # # Reemplazar valores
-# # print(animales.replace("Girafa", "Elefante"))
-# #
-# # print(4 / 2)
-# # num = -4.50255
-# # # convertir numero en string
-# # print(str(num))
-# # # Valor absoluto, nu mar negativos?
-# # # str(num)
-# # num = 5
-# #
-# # print(pow(num, 2))
-# #
-# # print(round(num))
-# #
-# # print(floor(3.7))  # Nearest [3]
-# # print(ceil(3.5))  # farest [4]
-# #
-# # # raiz cuadrada
-# #
-# # print(sqrt(9).__round__())  # perfecto
-# #
-# # # Input from users
-# #
-# # DNI = input("Introduce tu DNI: ")
-# # print(DNI)
-# #
-# # name = input("Introduce tu nombre: ")
-# #
-# # print(name, DNI)
-# #
-# # # /*    public static String nif(int dni) {
-# # #         char char = "TRWAGMYFPDXBNJZSQVHLCKE".charAt(dni % 23);
-# # #         return Integer.toString(dni) + charDni;
-# #
-# # # test = "kappa"
-# # # elem = test[0] # charAt(0), which is 'k' in this case
-# # #     }
-# # # // Division entera
+# from math import *
 #
-# # tuple no es modificable
+# # como importar cosas
+# output = 'Hola mundo'
+# print("Hola mundo!")
 #
-# coordenadas = [(5, 9), (15, 9)]  # lista de numeros
-# # list of tuples
-# print(coordenadas[0])
-# # funciones
+# number1 = 0
+# number2 = 55.45554
+# if number1 > number2:
+#     print(number1)
+# else:
+#     print(number2)
+# string = "jPython"
+#
+# print(string.upper() + "\n", number2)
+#
+# print(len(string))
+#
+# print(string[0] + string[5])
+#
+# print(string.index("P"))  # Guay
+#
+# # Replace funtion
+# animales = "Girafa Academy"
+# # Reemplazar valores
+# print(animales.replace("Girafa", "Elefante"))
+#
+# print(4 / 2)
+# num = -4.50255
+# # convertir numero en string
+# print(str(num))
+# # Valor absoluto, nu mar negativos?
+# # str(num)
+# num = 5
+#
+# print(pow(num, 2))
+#
+# print(round(num))
+#
+# print(floor(3.7))  # Nearest [3]
+# print(ceil(3.5))  # farest [4]
+#
+# # raiz cuadrada
+#
+# print(sqrt(9).__round__())  # perfecto
+#
+# # Input from users
+#
+# DNI = input("Introduce tu DNI: ")
+# print(DNI)
+#
+# name = input("Introduce tu nombre: ")
+#
+# print(name, DNI)
+#
+# # /*    public static String nif(int dni) {
+# #         char char = "TRWAGMYFPDXBNJZSQVHLCKE".charAt(dni % 23);
+# #         return Integer.toString(dni) + charDni;
+#
+# # test = "kappa"
+# # elem = test[0] # charAt(0), which is 'k' in this case
+# #     }
+# # // Division entera
+
+# tuple no es modificable
+
+coordenadas = [(5, 9), (15, 9)]  # lista de numeros
+# list of tuples
+print(coordenadas[0])
+# funciones
